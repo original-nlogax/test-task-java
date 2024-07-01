15f13cd74bb79d8f217740d46f10d27dd39d4d03
Георгий <egorizzee@gmail.com>
2024-07-01 18:31:34 +0300

    Add files via upload

---
+++ main.groovy
@@ -0,0 +1,17 @@
+import java.util.Map
+import java.util.HashMap
+import java.util.List
+import java.util.Arrays
+
+Map<Object, Integer> freq = new HashMap<>()
+List<Object> list = Arrays.asList(1, 3, 4, 5, 1, 5, 4)
+
+for (Object o : list) {
+    if (freq.containsKey(o)) {
+        freq.put(o, freq.get(o)+1)
+    } else {
+        freq.put(o, 1)
+    }
+}
+
+println(freq)
